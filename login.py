1c58b21b7ff3585adfc9b7ac6247985fefbc15f6
lwneal <lawrence@dropcam.com>
2013-12-09 19:54:21 -0800

    Better persistence for session cookies

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import getpass
-import json
+import pickle
 import requests
 from requests.utils import cookiejar_from_dict, dict_from_cookiejar
 
@@ -12,11 +12,10 @@ def get_cookiejar(username=None, password=None):
   """ Use a cached cookiejar if available, else prompt the user to log in """
   cached = load_from_file(COOKIE_CACHE)
   if cached:
-    return cookiejar_from_dict(cached)
+    return cached
   elif username and password:
-    return login_with_password(username=username, password=password)
-  else:
-    return login_with_password(**login_prompt())
+    return log_in_with_password(username=username, password=password)
+  return log_in_with_password(**login_prompt())
 
 def login_prompt():
   sys.stderr.write("Enter your Dropcam username: ")
@@ -25,24 +24,23 @@ def login_prompt():
   passwd = getpass.getpass('')
   return {'username': uname, 'password': passwd}
 
-def login_with_password(username, password):
+def log_in_with_password(username, password):
   with requests.session() as c:
     c.post(LOGIN_API, data={'username':username, 'password':password})
-    cookies = dict_from_cookiejar(c.cookies)
-    if not cookies:
-      raise Exception("Bad username or password")
-    save_to_file(cookies, COOKIE_CACHE)
-    return cookies
+    save_to_file(c.cookies, COOKIE_CACHE)
+    return dict_from_cookiejar(c.cookies)
 
-def save_to_file(stuff_to_save, fname):
+def save_to_file(data, fname):
   with open(fname, 'w') as outfile:
-    outfile.write(json.dumps(stuff_to_save))
+    pickle.dump(data, outfile)
   sys.stderr.write("NOTICE: Your session token has been saved in {0}\n".format(COOKIE_CACHE))
   
 def load_from_file(fname):
   try:
     with open(fname) as infile:
-      return json.loads(infile.read())
-  except IOError:
-    pass
-  return None
+      jar = pickle.load(infile)
+      if any([c.is_expired() for c in jar]):
+        return None
+      return dict_from_cookiejar(jar)
+  except Exception, e:
+    return None
