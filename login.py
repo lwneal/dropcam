b3496b7128bab28ff42b121645fcd1dd3ef8a511
lwneal <lawrence@dropcam.com>
2014-01-30 18:23:40 -0800

    Patch from @fyhuang to fix stale login cookies

--- login.py
+++ login.py
@@ -6,16 +6,25 @@ import requests
 from requests.utils import cookiejar_from_dict, dict_from_cookiejar
 
 LOGIN_API = 'https://www.dropcam.com/api/login.login'
+CAMERAS_API_PATH = "https://www.dropcam.com/api/v1/cameras.get_visible"
 COOKIE_CACHE = os.getenv("HOME") + '/.dropcam_cookie_cache'
 
 def get_cookiejar(username=None, password=None):
   """ Use a cached cookiejar if available, else prompt the user to log in """
+  def do_login():
+    if username and password:
+      return log_in_with_password(username=username, password=password)
+    return log_in_with_password(**login_prompt())
+
   cached = load_from_file(COOKIE_CACHE)
   if cached:
-    return cached
-  elif username and password:
-    return log_in_with_password(username=username, password=password)
-  return log_in_with_password(**login_prompt())
+    # Check if cached cookies still work
+    resp = requests.get(url=CAMERAS_API_PATH, params={'group_cameras': True}, cookies=cached)
+    if resp.ok and resp.json()['status'] == 0:
+      return cached
+    return do_login()
+  else:
+    return do_login()
 
 def login_prompt():
   sys.stderr.write("Enter your Dropcam username: ")
